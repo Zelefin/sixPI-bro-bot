1eb46932852f8bd6d1ee38d955ae50bff216d7d7
Zelefin <nagangoroh@gmail.com>
2024-07-09 12:19:47 +0300

    add scripts to create and restore db

---
+++ scripts/create_dump.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CONTAINER_NAME=SixPIBroBot
+export BOT_NAME=SixPIBroBot
+mkdir -p ./dump
+docker cp ${CONTAINER_NAME}:/usr/src/app/${BOT_NAME}/main.db ./dump/main.db
+
+if [ $? -eq 0 ]; then
+  echo "Dump successfully created."
+else
+  echo "Failed to create dump."
+  exit 1
+fi
